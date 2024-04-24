523e99c306ff105cbb7b6a56fbcec5243c5b8dd3
harristheharris <anthonyharris7777@gmail.com>
2024-04-23 21:41:00 -0400

    began getch api. next need to print the info we want, in the we want, on the page

--- Assets/js/5dayweather.js
+++ Assets/js/5dayweather.js
@@ -1,1 +1,44 @@
 var resultContentEl = document.querySelector('#result-content');
+
+function getParams() {
+    //we need to grab the search params from the url we redirected to 
+
+    var searchParamsArr = document.location.search;
+
+    //getting and formating the values
+    var query = searchParamsArr;
+
+    searchApi(query);
+}
+
+function searchApi(query){
+
+    var localQueryUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+
+    localQueryUrl = `${localQueryUrl}${query}`;
+
+    fetch(localQueryUrl)
+        .then(function (response){
+            if(!response.ok) {
+                throw response.json();
+            }
+
+            return response.json();
+        })
+        .then(function (locRes){
+            resultTextEl.textContent = locRes.search.query;
+            console.log(locRes);
+
+            if(!locRes.results.length) {
+                console.log(`no length on this array aka nothing in the array therefore no results`);
+                resultContentEl.innerHTML = `<h3>that ain't no city of mine! Try again!</h3>`;
+
+            } else {
+                resultContentEl.textContent = ``;
+                //we need to get one then all five
+            }
+        })
+        .catch(function (error) {
+            console.error(error);
+        })
+}
