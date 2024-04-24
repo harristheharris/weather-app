beecc0ca5fa968c69290a8d278344fb51a06bd3a
harristheharris <anthonyharris7777@gmail.com>
2024-04-23 20:40:35 -0400

    starting searching the api

---
+++ Assets/js/5dayweather.js
@@ -0,0 +1,1 @@
+var resultContentEl = document.querySelector('#result-content');
--- Assets/js/index.js
+++ Assets/js/index.js
@@ -5,11 +5,13 @@ var searchFormEl = document.querySelector('#search-form')
 //event function
 function handleSearchFormSubmit(event){
     //This event is on the submit button which by default will submit the form. We however do not want the form to be submitted right away. We first need to:
-    //Validate that the user inputted a city in the input field
+    //Validate that the user inputted something in the input field
         //and if they did not we want a message to the user
-    //then we need to make the call to the api using a custom link wit
+    //then we need to make the call to the api using a custom link with the city name
+    //and then redirect
     event.preventDefault();
 
+    //a variable to hold the value of the input
     var searchInputVal = document.querySelector('#search-input').value;
 
     if (!searchInputVal){
@@ -17,9 +19,11 @@ function handleSearchFormSubmit(event){
         return;
     }
 
+    //custom link with the city as the query
     var queryString = `./5dayweather.html?q=${searchInputVal}`;
 
-    locaction.assign(queryString);
+    //redirect
+    location.assign(queryString);
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
