3171c588915b5fd50567fa9a78a5fff185321dcf
harristheharris <anthonyharris7777@gmail.com>
2024-04-27 17:08:50 -0400

    update 042724

--- Assets/js/5dayweather.js
+++ Assets/js/5dayweather.js
@@ -14,7 +14,7 @@ function getParams() {
     searchApi(query);
 }
 
-function searchApi(query){
+function searchApi(query) {
     var apiKey = "2ffdf672e25a72f3635e2448e9356ebd"
     var localQueryUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 
@@ -22,14 +22,14 @@ function searchApi(query){
     console.log(localQueryUrl);
 
     fetch(localQueryUrl)
-        .then(function (response){
-            if(!response.ok) {
+        .then(function (response) {
+            if (!response.ok) {
                 throw response.json();
             }
 
             return response.json();
         })
-        .then(function (locRes){
+        .then(function (locRes) {
 
             console.log(locRes);
             console.log(locRes.city.name);
@@ -38,9 +38,9 @@ function searchApi(query){
             let cityName = locRes.city.name;
 
             // resultTextEl.textContent = cityName;
-            
 
-            if(!locRes.list.length) {
+
+            if (!locRes.list.length) {
                 console.log(`no length on this array aka nothing in the array therefore no results`);
                 resultContentEl.innerHTML = `<h3>that ain't no city of mine! Try again!</h3>`;
 
@@ -52,17 +52,17 @@ function searchApi(query){
                 for (const child of resultContentEl.children) {
 
                     //creating the varriable of the current child we are looping through
-                    
+
                     let currentChild = Array.prototype.indexOf.call(resultContentEl.children, child);
 
                     console.log(`This is current array #${currentChild + 1}`);
 
                     printResults(locRes.list, child, currentChild)
-                    
 
-                    
+
+
                 }
-    
+
 
             }
         })
@@ -71,7 +71,7 @@ function searchApi(query){
         })
 }
 
-function printResults(weatherData, child, currentChild){
+function printResults(weatherData, child, currentChild) {
     let poop1 = weatherData[1];
     let poop2 = weatherData[9];
     let poop3 = weatherData[17];
@@ -79,33 +79,33 @@ function printResults(weatherData, child, currentChild){
     let poop5 = weatherData[33];
     let eachDay = [poop1, poop2, poop3, poop4, poop5]
 
-let daysData = eachDay[currentChild]
-console.log(eachDay);
-// console.log(weatherData);
-// console.log(daysData);
-// console.log(daysData.main.temp);
-// console.log(daysData.weather[0].main);
-// console.log(daysData.main.humidity);
-// console.log(daysData.dt_txt);
-let tempData = daysData.main.temp;
-let skyData = daysData.weather[0].main;
-let dateData = daysData.dt_txt;
-let humidData = daysData.main.humidity;
-let windSpeedData = daysData.wind.speed;
-let iconData = daysData.weather[0].icon;
+    let daysData = eachDay[currentChild]
+    console.log(eachDay);
+    // console.log(weatherData);
+    // console.log(daysData);
+    // console.log(daysData.main.temp);
+    // console.log(daysData.weather[0].main);
+    // console.log(daysData.main.humidity);
+    // console.log(daysData.dt_txt);
+    let tempData = daysData.main.temp;
+    let skyData = daysData.weather[0].main;
+    let dateData = daysData.dt_txt;
+    let humidData = daysData.main.humidity;
+    let windSpeedData = daysData.wind.speed;
+    let iconData = daysData.weather[0].icon;
 
-    
+
     return child.innerHTML = `
     
     <div>
 
     <h4>${dateData}</h4>
     <div>
-    <p>Temparture: ${tempData} F</p>
-    <p>Skies: ${skyData}</p>
-    <p>Humidity: ${humidData}%</p>
-    <p>Wind Speed: ${windSpeedData}</p>
-    ${iconData}
+        <p>Temparture: ${tempData} F</p>
+        <p>Skies: ${skyData}</p>
+        <p>Humidity: ${humidData}%</p>
+        <p>Wind Speed: ${windSpeedData}</p>
+        ${iconData}
     </div>
         
 
