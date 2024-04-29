d552a74955e733f31739852985356a16cedf8333
harristheharris <anthonyharris7777@gmail.com>
2024-04-29 10:08:47 -0400

    add readme and further commetns

--- Assets/js/5dayweather.js
+++ Assets/js/5dayweather.js
@@ -18,6 +18,7 @@ function searchApi(query) {
     var apiKey = "2ffdf672e25a72f3635e2448e9356ebd"
     var localQueryUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 
+    //formatting the the url for the call
     localQueryUrl = `${localQueryUrl}${query}&cnt=40&appid=${apiKey}&units=imperial`;
     console.log(localQueryUrl);
 
@@ -35,11 +36,11 @@ function searchApi(query) {
             console.log(locRes.city.name);
             console.log(locRes.list.length);
             console.log(resultContentEl.children)
-            let cityName = locRes.city.name;
+            
 
             // resultTextEl.textContent = cityName;
 
-
+            //if there is nothing in the array then error
             if (!locRes.list.length) {
                 console.log(`no length on this array aka nothing in the array therefore no results`);
                 resultContentEl.innerHTML = `<h3>that ain't no city of mine! Try again!</h3>`;
@@ -57,7 +58,7 @@ function searchApi(query) {
 
                     console.log(`This is current array #${currentChild + 1}`);
 
-                    printResults(locRes.list, child, currentChild)
+                    printResults(locRes, child, currentChild)
 
 
 
@@ -71,12 +72,18 @@ function searchApi(query) {
         })
 }
 
+
+//funciton to print the results of our call
 function printResults(weatherData, child, currentChild) {
-    let poop1 = weatherData[1];
-    let poop2 = weatherData[9];
-    let poop3 = weatherData[17];
-    let poop4 = weatherData[25];
-    let poop5 = weatherData[33];
+    var cityName = weatherData.city.name;
+    
+
+    //this is a bad fix but basically we get 40 "timestamps" of the weather in in a given city. WE just want the next five days. This was the easiest way to get the next five days. Not a great fix though
+    let poop1 = weatherData.list[1];
+    let poop2 = weatherData.list[9];
+    let poop3 = weatherData.list[17];
+    let poop4 = weatherData.list[25];
+    let poop5 = weatherData.list[33];
     let eachDay = [poop1, poop2, poop3, poop4, poop5]
 
     let daysData = eachDay[currentChild]
@@ -87,6 +94,8 @@ function printResults(weatherData, child, currentChild) {
     // console.log(daysData.weather[0].main);
     // console.log(daysData.main.humidity);
     // console.log(daysData.dt_txt);
+
+    //intiallizng the data we want into variables
     let tempData = daysData.main.temp;
     let skyData = daysData.weather[0].main;
     let dateData = daysData.dt_txt;
@@ -94,12 +103,14 @@ function printResults(weatherData, child, currentChild) {
     let windSpeedData = daysData.wind.speed;
     let iconData = daysData.weather[0].icon;
 
-
+    //priont time
     return child.innerHTML = `
     
+    
     <div>
-
-    <h4>${dateData}</h4>
+    
+    <h4>${cityName}</h4>
+    <div>${dateData}</div>
     <div>
         <p>Temparture: ${tempData} F</p>
         <p>Skies: ${skyData}</p>
